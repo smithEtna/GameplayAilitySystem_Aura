ca44ee86e05762c948b9b21637405213c769e1e1
史密斯工程狮 <13614909474@163.com>
2024-06-08 20:32:15 +0800

    初始化项目

---
+++ Source/Aura.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class AuraTarget : TargetRules
+{
+	public AuraTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		ExtraModuleNames.Add("Aura");
+	}
+}
