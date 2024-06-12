9ce08dd8cce03386b0bda915cadc11e7f4053e3d
smithEtna <13614909474@163.com>
2024-06-12 21:58:53 +0800

    添加血量UI 添加魔法量UI

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "Actor/AuraEffectActor.h"
 #include "AbilitySystemComponent.h"
 #include "AbilitySystemInterface.h"
-#include "Actor/AuraEffectActor.h"
 #include "Components/ShapeComponent.h"
 #include "AbilitySystem/AuraAttributeSet.h"
 
---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
+
+//void UAuraUserWidget::WidgetControllerSet()
+//{
+//}
